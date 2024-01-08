faf42c86aeca08aedb6321de8b86d105ab0cb352
shoalam <shoalamdu@gmail.com>
2024-01-08 12:00:47 +0600

    Page title updated

--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -1,12 +1,14 @@
 import { Helmet } from "react-helmet";
 import React from 'react'
 
-export const PageTitle = ({title}) => {
+export const PageTitle = ({title, featureImage, metaDescription}) => {
   return (
       <Helmet>
           <meta charSet="utf-8" />
           <title>{title}</title>
-          <link rel="canonical" href="http://mysite.com/example" />
+          <meta property="og:title" content={title} />
+          <meta property="og:image" content={featureImage} />
+          <meta property="og:description" content={metaDescription} />    
       </Helmet>
   )
 }
